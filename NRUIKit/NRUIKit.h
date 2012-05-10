b226896694cd3aadc92cec0cc4e1b991dfaa4ca6
andy@niveusrosea.com <andy@niveusrosea.com>
2012-05-10 09:44:11 +0100

    Initial Commit

---
+++ NRUIKit/NRUIKit.h
@@ -0,0 +1,13 @@
+//
+//  NRUIKit.h
+//  NRUIKit
+//
+//  Created by Andy Yardley on 10/05/2012.
+//  Copyright (c) 2012 Venalicium Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NRUIKit : NSObject
+
+@end
---
+++ NRUIKit/NRUIKit.m
@@ -0,0 +1,13 @@
+//
+//  NRUIKit.m
+//  NRUIKit
+//
+//  Created by Andy Yardley on 10/05/2012.
+//  Copyright (c) 2012 Venalicium Ltd. All rights reserved.
+//
+
+#import "NRUIKit.h"
+
+@implementation NRUIKit
+
+@end
